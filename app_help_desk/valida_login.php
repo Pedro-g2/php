745636ea6874667cbf4cdc723eacfa5c0a34f59f
Pedro Gonçalves <Pedro-g2>
2025-01-27 18:03:53 -0300

    adicionado projeto help desk

---
+++ app_help_desk/valida_login.php
@@ -0,0 +1,33 @@
+<?php
+
+    $usuarios_app = array(
+        array("email"=>"adm@email.com","senha"=>"123456"),
+        array("email"=>"usr@email.com","senha"=>"abcdef")
+    );    
+
+    $usr_email = $_POST["email"];
+    $usr_senha = $_POST["password"];
+
+    $valida_email = false;
+    $valida_senha = false;
+
+    for($cont = 0; $cont < count($usuarios_app); $cont++){
+       
+        if($usr_email == $usuarios_app[$cont]["email"]){
+            $valida_email = true;
+            if($usr_senha == $usuarios_app[$cont]["senha"]){
+                $valida_senha = true;
+            }
+        }
+    }
+
+    if(!$valida_email){
+        header("Location: index.php?email=erro");
+    }else if(!$valida_senha){
+        header("Location: index.php?senha=erro");
+    }
+
+    
+    
+   
+        
